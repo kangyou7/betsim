81d44a4d9cb79239a4289142064e8eda42444e58
kangyou.choi <kangpele@sptek.co.kr>
2017-01-03 16:10:56 +0900

    init

---
+++ src/main/java/com/dongak/betinfo/BetsimApplication.java
@@ -0,0 +1,12 @@
+package com.dongak.betinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetsimApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dongak/betinfo/BetsimApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dongak.betinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BetsimApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
